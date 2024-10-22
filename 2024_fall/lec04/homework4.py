090e99661335d0571c112ec838319f127e884cf0
m23w0015 <2047932525@qq.com>
2024-10-22 10:34:16 +0900

    homework4.py

--- 2024_fall/lec04/homework4.py
+++ 2024_fall/lec04/homework4.py
@@ -5,7 +5,12 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
+    import importlib, homework4
+    importlib.reload(homework4)
+
+    print(homework4.list_to_dict([1, 3.14, "hello", True])) 
+    print(homework4.list_to_dict(["a", "a", "a"]))           
+    print(homework4.list_to_dict([]))                       
 
 
 
